91b88a60e5807fc063307902451a4dc2bf27bf27
Kamva <mtikasoga96@gmail.com>
2024-07-22 22:15:17 +0200

    add authentication

--- my-nextjs-app/app/(auth)/layout.tsx
+++ my-nextjs-app/app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <main className='flex min-h-screen w-full items-center justify-center'>
+    <main>
       {children}
     </main>
   )
