ff9ad3b881d8ad6cd0ae7744c4e184f37159dfff
Kamva <mtikasoga96@gmail.com>
2024-07-22 22:26:13 +0200

    implement root and meta data

--- my-nextjs-app/app/layout.tsx
+++ my-nextjs-app/app/layout.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {ClerkProvider, SignInButton, SignedIn, SignedOut} from '@clerk/nextjs'
-import './globals.css';
+import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Marketer Flow",
+  description: `A dedicated platform for digital marketers and SEO experts,
+   providing them with a space to share knowledge, tools, 
+   and resources, and to stay updated with industry trends.
+  Sharing marketing strategies, discussing SEO techniques, 
+  seeking advice on analytics, and staying updated with industry trends.
+  Features: Marketing guides, SEO tools, analytics dashboards, and trend analysis
+  `,
+};
 
-export default function RootLayout({children,}: {
-  children: React.ReactNode
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -13,11 +32,10 @@ export default function RootLayout({children,}: {
           <SignedOut>
             <SignInButton />
           </SignedOut>
-          <SignedIn>
-          </SignedIn>
+          <SignedIn></SignedIn>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
